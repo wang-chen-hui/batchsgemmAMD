132e7b05b881413fa0bb6c1f4d783ddda270e6be
guocong <guocong@sjtu.edu.cn>
2019-12-23 20:47:53 +0800

    Nvidia GPU test

---
+++ nvidia_gpu_test/include/sgemm_strided_batched.h
@@ -0,0 +1,105 @@
+
+/**/
+#include <iostream>
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "cuda_runtime_api.h"
+
+#ifndef SGEMM_STRIDED_BATCHED_
+#define SGEMM_STRIDED_BATCHED_
+
+typedef enum sgemm_operation_
+{
+    operation_none = 0,
+    operation_transpose = 1,
+    operation_conjugate_transpose = 2
+} sgemm_operation;
+
+__global__ void ReferenceGemm_kernel(
+    int M,
+    int N,
+    int K,
+    float alpha,
+    const float *A,
+    int lda,
+    const float *B,
+    int ldb,
+    float beta,
+    float *C,
+    int ldc)
+{
+
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+    if (i < M && j < N)
+    {
+        float accumulator = 0;
+
+        for (int k = 0; k < K; ++k)
+        {
+            accumulator += A[i + k * lda] * B[k + j * ldb];
+        }
+
+        C[i + j * ldc] = alpha * accumulator + beta * C[i + j * ldc];
+    }
+}
+
+void sgemm_strided_batched(sgemm_operation trans_a,
+                           sgemm_operation trans_b,
+                           int m,
+                           int n,
+                           int k,
+                           const float *alpha,
+                           const float *A,
+                           int lda,
+                           int stride_a,
+                           const float *B,
+                           int ldb,
+                           int stride_b,
+                           const float *beta,
+                           float *C,
+                           int ldc,
+                           int stride_c,
+                           int batch_count)
+{
+    //cout << "compute sgemm stride batch" << endl;
+
+    dim3 block(16, 16);
+    dim3 grid(
+        (m + block.x - 1) / block.x,
+        (n + block.y - 1) / block.y);
+
+    if (trans_a == operation_none && trans_b == operation_none)
+    {
+        cout << "NO transpose" << endl;
+    }
+    else
+    {
+        cout << "Transpose!" << endl;
+    }
+
+    for (int i = 0; i < batch_count; i++)
+    {
+        cout << "Batched  : " << i << endl;
+
+        A += stride_a;
+        B += stride_b;
+        C += stride_c;
+        ReferenceGemm_kernel<<<grid, block>>>(
+            m,
+            n,
+            k,
+            *alpha,
+            A,
+            lda,
+            B,
+            ldb,
+            *beta,
+            C,
+            ldc);
+    }
+
+    //cout << "compute sgemm stride batch" << endl;
+}
+#endif
