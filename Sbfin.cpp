20a651abab42701836e4295291b920b1142ab4e1
wang-chen-hui <47982075+wang-chen-hui@users.noreply.github.com>
2020-04-29 16:20:04 +0800

    Add files via upload

---
+++ Sbfin.cpp
@@ -0,0 +1,167 @@
+#ifndef SGEMM_STRIDED_BATCHED_
+#define SGEMM_STRIDED_BATCHED_
+
+#include<iostream>
+#include "hip/hip_runtime.h"
+#include <time.h> 
+
+using namespace std;
+
+#define BLOCK_SIZE 16
+typedef enum sgemm_operation_ {
+    operation_none      = 0, 
+    operation_transpose  = 1,
+    operation_conjugate_transpose = 2
+} sgemm_operation;
+
+__global__ void ReferenceGemm_kernel(
+    int M,
+    int N,
+    int K,
+    float alpha,
+    const float *A,
+    int lda,
+    const float *B,
+    int ldb,
+    float beta,
+    float *C,
+    int ldc)
+    {
+    int col = hipThreadIdx_x;
+    int row = hipThreadIdx_y;
+    int i = hipThreadIdx_x + hipBlockIdx_x * BLOCK_SIZE;
+    int j = hipThreadIdx_y + hipBlockIdx_y * BLOCK_SIZE;
+    __shared__ float As[BLOCK_SIZE][BLOCK_SIZE + 1];
+    __shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE + 1];
+
+    float accumulator = 0;
+    int k = 0;
+    int res = K % BLOCK_SIZE;
+
+    for (k = 0; k < K - res; k += BLOCK_SIZE)
+    {
+
+        As[row][col] = A[i + (k + row) * lda];
+        Bs[row][col] = B[k + col + j * ldb];
+        __syncthreads();
+
+        for (int e = 0; e < BLOCK_SIZE; ++e)
+            accumulator += As[e][col] * Bs[row][e];
+        __syncthreads();
+    }
+
+    if (res != 0)
+    {
+        if (row < res)
+            As[row][col] = A[i + (k + row) * lda];
+        if (col < res)
+            Bs[row][col] = B[k + col + j * ldb];
+
+        __syncthreads();
+        for (int e = 0; e < res; ++e)
+            accumulator += As[e][col] * Bs[row][e];
+        __syncthreads();
+    }
+
+    if (i < M && j < N)
+    {
+        C[i + j * ldc] = alpha * accumulator + beta * C[i + j * ldc];
+    }
+        // int i = hipThreadIdx_x + hipBlockIdx_x * hipBlockDim_x;
+        // int j = hipThreadIdx_y + hipBlockIdx_y * hipBlockDim_y;
+        // if (i < M && j < N) 
+        // {
+        // float accumulator = 0;
+        // for (int k = 0; k < K; ++k)
+        // {
+        //     accumulator += A[i + k * lda] * B[k + j * ldb];
+        // }
+        // C[i + j * ldc] = alpha * accumulator + beta * C[i + j * ldc];
+        // }
+    }
+void sgemm_strided_batched(sgemm_operation trans_a,
+                           sgemm_operation trans_b,
+                           int m,
+                           int n,
+                           int k,
+                           const float* alpha,
+                           const float* A,
+                           int lda,
+                           int stride_a,
+                           const float* B,
+                           int ldb,
+                           int stride_b,
+                           const float* beta,
+                           float* C,
+                           int ldc,
+                           int stride_c,
+                           int batch_count)
+{
+    //cout << "compute sgemm stride batch" << endl;
+    dim3 block(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 grid(
+        (m + block.x - 1) / block.x,
+        (n + block.y - 1) / block.y);
+
+    hipStream_t stream[10];
+    for(int i = 0; i < 10; i++)
+    {
+
+        hipStreamCreate(&stream[i]);
+    }
+    hipStreamCreateWithFlags(&stream2, hipStreamNonBlocking);
+    hipStreamCreateWithFlags(&stream3, hipStreamNonBlocking);
+    hipStreamCreateWithFlags(&stream4, hipStreamNonBlocking);
+    hipStreamCreateWithFlags(&stream5, hipStreamNonBlocking);
+    hipStreamCreateWithFlags(&stream6, hipStreamNonBlocking);
+    hipStreamCreateWithFlags(&stream7, hipStreamNonBlocking);
+    hipStreamCreateWithFlags(&stream8, hipStreamNonBlocking);
+    hipStreamCreateWithFlags(&stream9, hipStreamNonBlocking);
+    hipStreamCreateWithFlags(&stream10, hipStreamNonBlocking);
+
+
+    /*if (trans_a == operation_none && trans_b == operation_none)
+    {
+        cout << "NO transpose" << endl;
+    }
+    else
+    {
+        cout << "Transpose!" << endl;
+    }
+    */
+    for (int i = 0; i < batch_count; i++)
+    {
+        hipLaunchKernelGGL(ReferenceGemm_kernel, grid, block, 0 , stream[i],
+            m,
+            n,
+            k,
+            *alpha,
+            A,
+            lda,
+            B,
+            ldb,
+            *beta,
+            C,
+            ldc);
+        A += stride_a;
+        B += stride_b;
+        C += stride_c;
+    }
+
+
+    for(int i = 0; i < 10; i++)
+    {
+    hipStreamDestroy(&stream[i]);
+    }
+    // hipStreamDestroy(stream1);
+    // hipStreamDestroy(stream2);
+    // hipStreamDestroy(stream3);
+    // hipStreamDestroy(stream4);
+    // hipStreamDestroy(stream5);
+    // hipStreamDestroy(stream6);
+    // hipStreamDestroy(stream7);
+    // hipStreamDestroy(stream8);
+    // hipStreamDestroy(stream9);
+    // hipStreamDestroy(stream10);
+}
+#endif
